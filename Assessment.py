6620b8b2a459295d84690221e14035b0cb0d3fbd
wbhsLoganF <lf21549@my.westlake.school.nz>
2024-03-19 14:20:20 +1300

    19/03

--- Assessment.py
+++ Assessment.py
@@ -1,59 +1,74 @@
 import easygui
+
+"""Setting out the nested dictionary, which houses all tasks"""
 tasks = {
-    "T1":{
+    "1":{
         "Title": "Design Homepage",
         "Description": "Create a mockup of the homepage",
         "Assignee": "JSM",
-        "Priority": "3",
+        "Priority": 3,
         "Status": "In Progress",
     },
-    "T2":{
+    "2":{
         "Title": "Implement Login Page",
         "Description": "Create the Login page for the website",
         "Assignee": "JSM",
-        "Priority": "3",
+        "Priority": 3,
         "Status": "Blocked",
     },
-    "T3":{
+    "3":{
         "Title": "Fix Navigation Menu",
         "Description": "Fix the navigation menu to be more user friendly",
         "Assignee": "None",
-        "Priority": "1",
+        "Priority": 1,
         "Status": "Not Started",
     },
-    "T4":{
+    "4":{
         "Title": "Add Payment Processing",
         "Description": "Implement payment processing for the website",
         "Assignee": "JLO",
-        "Priority": "2",
+        "Priority": 2,
         "Status": "In Progress",
     },
-    "T5":{
+    "5":{
         "Title": "Create an About Us Page",
         "Description": "Create a page with information about the company",
         "Assignee": "BDI",
-        "Priority": "1",
+        "Priority": 1,
         "Status": "Blocked",
     }
 }
 
+"""Setting out another nested dictionary, which details all members of 
+the company"""
 members = {
     "JSM":{
         "Name": "John Smith",
-        "Email": "John@techvision.com"
+        "Email": "John@techvision.com",
+        "Tasks Assigned": ""
     },
     "JLO":{
         "Name": "Jane Love",
-        "Email": "Jane@techvision.com"
+        "Email": "Jane@techvision.com",
+        "Tasks Assigned": ""
     },
     "BDI":{
         "Name": "Bob Dillon",
-        "Email": "Bob@techvision.com"
+        "Email": "Bob@techvision.com",
         "Tasks Assigned": ""
-    },
+    }
 }
 
-task_number = 0
+
+"""Establishing variables which are called for later on in the code"""
+member_codes = []
+for codes in members: 
+    member_codes.append(codes)
+member_codes.append("None")
+
+
+
+"""All functions will now be added"""
 
 def print_whole():
     """Displays entire nested dictionary in one msgbox by adding each
@@ -61,52 +76,36 @@ def print_whole():
 
     output = ""
     for item, item_info in tasks.items():
-        output += (f"\ntask: {item} \n\n")
+        output += (f"\nTask Number: {item} \n\n")  
         for key in item_info:
             output += (f"{key}: {item_info[key]}  \n")
     title = "LIST"
     easygui.msgbox(output, title)
 
-def search_option():
-    """Allows the user to search a certain task and displays all
-    aspects inside that title""" 
 
-    output = "" 
-    categories = [] 
-    title = "SEARCH" 
-    msg = "Click on the title you would like displayed" 
-    #adding the movie titles from the dictionary to a list
-    for titles in tasks: 
-        categories.append(titles) 
-
-    #getting the users choice
-    search_title = easygui.buttonbox(msg, title, categories) 
-    for items in tasks[search_title]: 
-        output += f"\n{items}: {tasks[search_title][items]}" 
-    title = f"{search_title}"
-    easygui.msgbox(output, title) 
-
-
-def add_entry():
+def add_task():
     """Allows the user to add a new task to the database"""
 
-    #getting all information for the new entry from user
+    #Getting all information for the new task from the user
     msg = "Enter a title for the task"
     title = easygui.enterbox(msg)
-    msg = "Enter the a description of the task:"
+    msg = "Enter a description of the task:"
     description = easygui.enterbox(msg)
-    msg = "Enter select a task member to assign to the task:"
-    assignee = easygui.buttonbox(msg)
+    msg = "Select a team member to assign to the task:"
+    blank = ""
+    choices = member_codes
+    assignee = easygui.buttonbox(msg,blank,choices)
     msg ="Enter the priority of the task (1-3):"
     priority = easygui.integerbox(msg)
     msg = "Enter the status of the task:"
     status = easygui.enterbox(msg)
 
-    #add the new entry to the tasks dictionary
+    #Add the new entry to the tasks dictionary
+    task_number = 1
     for i in tasks:
         task_number += 1
-    task_entry = "T" + f"{task_number}"
-    tasks[f"T{task_entry}"] = {
+        
+    tasks[task_number] = {
         "Title": title,
         "Description": description,
         "Assignee": assignee,
@@ -125,18 +124,18 @@ def delete_task():
     categories = [] 
     title = "SELECT" 
     msg = "Click on the task you would like to remove" 
-    #adding the tasks from the dictionary to a list
+    #Adding the tasks from the dictionary to a list
     for task in tasks: 
         categories.append(task) 
 
-    #getting the users choice
+    #Getting the users choice
     deleted_task = easygui.buttonbox(msg, title, categories) 
     msg= f"Are you sure you want to delete {deleted_task}?"
     title = "CONFIRM"
     choices = ["Yes","No"]
     confirm = easygui.buttonbox(msg,title,choices)
 
-    #confirming the choice
+    #Confirming the choice
     if confirm == "Yes":
         del tasks[delete_task]
         msg = "Done!"
@@ -148,20 +147,27 @@ def delete_task():
         easygui.msgbox(msg,title)
 
 
-#main menu with all options available as a buttonbox
+def change_status():
+    print("e")
+def change_priority():
+    print("e")
+def change_assignee():
+    print("e")
+
+
+
+#Main menu with all options available as a buttonbox
 while True:
     msg = "Please choose an option"
     title = "MAIN MENU"                                                            
-    choices = ["Print whole", "Search", "Update tasks", "Quit"]
+    choices = ["Print whole", "Update tasks", "Quit"]
     main_menu = easygui.buttonbox(msg,title,choices)
 
-    #checking which function to run based off input from main menu
-    if main_menu == "Print Whole":
+    #Checking which function to run based off input from main menu
+    if main_menu == "Print whole":
         print_whole() 
-    elif main_menu == "Search":
-        search_option()
 
-    #a seperate menu if the user wishes to change the dictionary,
+    #A seperate menu if the user wishes to change the dictionary,
     #as to not overcrowd the buttonbox
     elif main_menu == "Update tasks":
         msg = "What do you want to do?"
